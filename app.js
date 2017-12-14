ab28116092ecf3d2a3a7cb79047a526dc711611c
Anil <abasdeo29@gmail.com>
2017-12-14 14:19:39 -0500

    Added event listeners

--- app.js
+++ app.js
@@ -1,3 +1,8 @@
+/*
+////////////////////////////////////////////////////////////////////////////////////
+// Section 6.68
+////////////////////////////////////////////////////////////////////////////////////
+
 // IIFE == Immediately Invoked Function Expression
 // 			== anonymous function wrapped in parentheses
 var budgetController = (function() {
@@ -6,10 +11,57 @@ var budgetController = (function() {
   var add = function(a) {
     return x + a;
   };
-
+  // Writing like this gives us access from the outside
   return {
     publicTest: function(b) {
-      console.log(add(b));
+      return add(b);
     }
   };
+})();
+
+var UIController = (function() {
+  // Some code
+})();
+
+var controller = (function(budgetCtrl, UICtrl) {
+  var z = budgetCtrl.publicTest(5);
+
+  return {
+    anotherPublic: function() {
+      console.log(z);
+    }
+  };
+})(budgetController, UIController);
+*/
+
+////////////////////////////////////////////////////////////////////////////////////
+// Section 6.69
+////////////////////////////////////////////////////////////////////////////////////
+
+var budgetController = (function() {})();
+
+var UIController = (function() {})();
+
+// GLOBAL APP CONTROLLER
+var controller = (function(budgetCtrl, UICtrl) {
+  var ctrlAddItem = function() {
+    // Get the field input data
+
+    // Add the item to the budget controller
+
+    // Add the item to the UI
+
+    // Calculate the budget
+
+    // Display the budget on the UI
+    console.log('It works');
+  };
+
+  document.querySelector('.add__btn').addEventListener('click', ctrlAddItem);
+
+  document.addEventListener('keypress', function(event) {
+    if (event.keyCode == 13 || event.which == 13) {
+      ctrlAddItem();
+    }
+  });
 })();
